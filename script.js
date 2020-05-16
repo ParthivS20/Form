8359c5c33c49a9b9dc82eb4b234e83f5fd7a106f
Parthiv Shah <42523433+ParthivS20@users.noreply.github.com>
2020-05-16 13:18:30 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+   function getUA() {
+       console.log("device typing");
+        let device = "Unknown";
+        const ua = {
+            "Generic Linux": /Linux/i,
+            "Android": /Android/i,
+            "BlackBerry": /BlackBerry/i,
+            "Bluebird": /EF500/i,
+            "Chrome OS": /CrOS/i,
+            "Datalogic": /DL-AXIS/i,
+            "Honeywell": /CT50/i,
+            "iPad": /iPad/i,
+            "iPhone": /iPhone/i,
+            "iPod": /iPod/i,
+            "macOS": /Macintosh/i,
+            "Windows": /IEMobile|Windows/i,
+            "Zebra": /TC70|TC55/i,
+        }
+        Object.keys(ua).map(v => navigator.userAgent.match(ua[v]) && (device = v));
+        document.getElementById("type").value=device;
+    }
+
+
+
+
